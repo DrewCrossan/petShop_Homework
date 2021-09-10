9223541133456fa66ddb27aa9a9f4374dd5333bf
Andrew Crossan <andrewcrossanit@gmail.com>
2021-09-10 14:45:09 +0100

    test 2, 3 and 4 passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,5 +1,13 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(shops):
-    return shops["name"]
+def get_pet_shop_name(shop):
+    return shop["name"]
 
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"]
+
+def add_or_remove_cash(shop, cash):
+    shop["admin"]["total_cash"] += cash
+
+def get_pets_sold(shop):
+    return shop["admin"]["pets_sold"]
